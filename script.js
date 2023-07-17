fd0d056da95ce855913e1f4ce5393e9dec3beb52
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-07-17 17:50:59 +0100

    idan

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let inputEl = document.getElementById("input-el");
 let buttonEl = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 console.log(localStorage.getItem("myLeads"));
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 buttonEl.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
 });
 

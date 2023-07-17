5864f647eda26439cd881fc84ac54d93209de2d6
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-07-17 19:18:32 +0100

    idann

--- script.js
+++ script.js
@@ -4,28 +4,18 @@ const buttonEl = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("dlt-btn");
 console.log(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabs = [{ url: "kkkkkkkkkkkkkkkkkkkkkkk" }];
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
-deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  renderLeads();
-});
-buttonEl.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
-});
-
-function renderLeads() {
+function render(leads) {
   let listItem = "";
 
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItem +=
       "<li> <a  target='_blank' href=' " +
       myLeads[i] +
@@ -35,3 +25,18 @@ function renderLeads() {
   }
   ulEl.innerHTML = listItem;
 }
+tabBtn.addEventListener("click", function () {
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+});
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+buttonEl.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
